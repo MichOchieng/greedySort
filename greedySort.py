e21590691d230fba750126922a4802f348b09321
MichOchieng <MichOchieng@gmail.com>
2022-02-20 18:33:39 -0800

    Added skeleton

---
+++ greedySort.py
@@ -0,0 +1,12 @@
+import re
+
+class greedySort:
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    program = greedySort()
+    greedySort.run()

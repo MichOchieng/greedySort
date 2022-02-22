f5bb6976d3980c1b08c48078eb4ba01319501287
MichOchieng <MichOchieng@gmail.com>
2022-02-22 01:11:41 -0800

    Added initialize function

--- greedySort.py
+++ greedySort.py
@@ -1,12 +1,31 @@
 import re
+import sys
 
 class greedySort:
     def __init__(self) -> None:
-        pass
+        self.initialize()
+
+    PERMUTATION = (list)
+
+    def initialize(self):
+        try:
+            with open(sys.argv[1]) as file:
+                lines            = file.readlines()
+                # Convert strings into ints
+                self.PERMUTATION = list(
+                            map(
+                                int,
+                                lines[1].replace('(','').replace(')','').split()
+                                )
+                            )
+                print(self.PERMUTATION)
+        except OSError:
+            print("Could not open file " + sys.argv[1])
+        except IndexError:
+            print("Please enter a file name.")
 
     def run(self):
         pass
 
 if __name__ == "__main__":
     program = greedySort()
-    greedySort.run()

fadcb6b56dffb492134f9c23fe1c4c9629d0dde4
MichOchieng <MichOchieng@gmail.com>
2022-02-23 20:45:32 -0800

    Added displayPermutation function

--- greedySort.py
+++ greedySort.py
@@ -6,30 +6,36 @@ class greedySort:
         self.initialize()
 
     PERMUTATION = (list)
+    STEP_NUMBER = 0
 
     def initialize(self):
         try:
             with open(sys.argv[1]) as file:
                 lines            = file.readlines()
-                # Convert strings into ints
-                self.PERMUTATION = list(
-                            map(
-                                int,
-                                lines[1].replace('(','').replace(')','').split()
-                                )
-                            )
-                self.PERMUTATION.insert(0,0)
-                self.PERMUTATION.append(len(self.PERMUTATION))
+                self.PERMUTATION = lines[1].replace('(','').replace(')','').split()
+                # self.PERMUTATION.insert(0,0)
+                # self.PERMUTATION.append(len(self.PERMUTATION))
         except OSError:
             print("Could not open file " + sys.argv[1])
         except IndexError:
             print("Please enter a file name.")
 
     def greedySort(self):
-        print(self.PERMUTATION)
+        n = len(self.PERMUTATION)
+        for x in range(n):
+            if(x != int(self.PERMUTATION[x])):
+                # Find where the correct value is 
+                # Reverse everything in the path up to that value
+                pass
+
+    def displayPermutation(self):
+        self.STEP_NUMBER += 1
+        print("(" + str(self.STEP_NUMBER) + ")", end=" ")
+        for val in self.PERMUTATION:
+            print(val, end=" ")
 
     def run(self):
-        self.greedySort()
+        self.displayPermutation()
 
 if __name__ == "__main__":
     program = greedySort()
